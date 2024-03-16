f5bddb3ab39b9e0cdfb0f9171c8082b8aa5c0f0f
neemiasmartins <86680482+neemiasmartins@users.noreply.github.com>
2024-03-16 11:35:50 -0300

    netlify não compilou - deletando ultimas mudanças

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,14 +8,3 @@ export default defineConfig({
 	site: 'https://neemias.org',
 	integrations: [mdx(), sitemap()],
 });
-// https://docs.astro.build/en/guides/internationalization/
-import { defineConfig } from "astro/config";
-export default defineConfig({
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "pt-br"],
-    routing: {
-        prefixDefaultLocale: false
-    }
-  }
-});
